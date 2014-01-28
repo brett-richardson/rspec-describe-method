1bf4c6a7541cdc1e2baabd8a53e4a5dc5c55f04f
Brett Richardson <brett.richardson.nz@gmail.com>
2014-01-28 01:19:34 +0100

    Fix broken require configure

--- lib/rspec/describe_method.rb
+++ lib/rspec/describe_method.rb
@@ -5,4 +5,4 @@ end
 
 require 'rspec/describe_method/version'
 require 'rspec/describe_method/class_extensions'
-require 'rspec/describe_method/configure'
+# require 'rspec/describe_method/configure'
